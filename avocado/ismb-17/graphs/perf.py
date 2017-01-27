c1e539122e0ebaa0c823b55d13ca34fd573f01aa
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2017-01-26 22:41:08 -0800

    Cleaned up spacing in perf figure.

--- avocado/ismb-17/graphs/perf.py
+++ avocado/ismb-17/graphs/perf.py
@@ -9,7 +9,7 @@ score = (18, 43)
 call = (0.5, 7)
 precall = (20, 45)
 ind = np.arange(N)    # the x locations for the groups
-width = 0.4       # the width of the bars: can also be len(x) sequence
+width = 0.5       # the width of the bars: can also be len(x) sequence
 
 p1 = plt.bar(ind, discovery, width, color='#ff0000')
 p2 = plt.bar(ind, score, width, bottom=discovery, color='#00ff00')
@@ -17,7 +17,8 @@ p3 = plt.bar(ind, call, width, bottom=precall, color='#0000ff')
 
 plt.ylabel('Time (min)')
 plt.title('Runtime per stage')
-plt.xticks((0.2, 1.2), ('Single', 'gVCF'))
+plt.xlim(-0.4, 2.0)
+plt.xticks((0.25, 1.25), ('Single', 'gVCF'))
 plt.yticks(np.arange(0, 56, 5))
 plt.legend((p1[0], p2[0], p3[0]), ('Discovery', 'Scoring', 'Call & Filter'), loc = 2)
 
